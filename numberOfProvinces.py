1a2b098b580f1e4345c5cc3f6ffd9acf8b777210
chrisAlgos <96036905+chrisAlgos@users.noreply.github.com>
2022-01-01 03:34:53 +0900

    Update numberOfProvinces.py

--- numberOfProvinces.py
+++ numberOfProvinces.py
@@ -1,1 +1,26 @@
+'''
+Question: Number of Provinces 
+https://leetcode.com/problems/number-of-provinces/
+A province is a group of directly or indirectly connected cities (basically a group of connected nodes)
+Count number of provinces in the graph.
+i.e. 3 disconnected nodes = 3 provinces 
+'''
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
 
+        visited, n = set(), len(isConnected)
+        def dfs(node): 
+            for i in range(n): 
+                if i == node: continue 
+                if i in visited: continue 
+                if isConnected[node][i] == 1: 
+                    visited.add(i)
+                    dfs(i)
+        
+        count = 0
+        for node in range(n): #n = number of nodes in the graph
+            if node in visited: continue 
+            visited.add(node) #the node itself will be trivially counted in the for loop outside of dfs
+            dfs(node)
+            count += 1
+        return count
