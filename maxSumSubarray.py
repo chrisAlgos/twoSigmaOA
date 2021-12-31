05f401b2716febf6f4dc2a6dda4519113620d9d4
chrisAlgos <96036905+chrisAlgos@users.noreply.github.com>
2022-01-01 03:18:45 +0900

    Update maxSumSubarray.py

--- maxSumSubarray.py
+++ maxSumSubarray.py
@@ -5,9 +5,9 @@ Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 
-
 Time | Space = O(N) | O(1)
-'''
+
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxSum, runSum = -float('inf'), 0 
@@ -18,3 +18,14 @@ class Solution:
             runSum += num
             maxSum = max(maxSum, runSum)
         return maxSum 
+'''
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        maxResult, maxRunSum = -float('inf'), -float('inf')
+        
+        for num in nums: 
+            maxRunSum = max(maxRunSum+num, num) # at each pass, see which one is greater: maxRunSum up until i plus num or num?
+            maxResult = max(maxResult, maxRunSum) # at each pass, update maxResult
+        return maxResult
