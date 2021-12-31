66db46d3d4b95b62f91bc0a8072f100ec7f5ac9d
chrisAlgos <96036905+chrisAlgos@users.noreply.github.com>
2021-12-31 22:26:59 +0900

    Update bestTimeToBuyAndSellStockIII.py

--- bestTimeToBuyAndSellStockIII.py
+++ bestTimeToBuyAndSellStockIII.py
@@ -1,1 +1,17 @@
-
+'''
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        firstBuyFirstSellProfit, secondBuySecondSellProfit = 0, 0 
+        firstBuy, secondBuy = float('inf'), float('inf')
+        
+        for i in range(len(prices)): 
+            price = prices[i]
+            firstBuy = min(firstBuy, price)
+            firstBuyFirstSellProfit = max(firstBuyFirstSellProfit, price-firstBuy)
+            #firstBuyFirstSellProfit is used to lower the cost of secondBuy
+            secondBuy = min(secondBuy, price - firstBuyFirstSellProfit)
+            secondBuySecondSellProfit = max(secondBuySecondSellProfit, price-secondBuy)
+        return secondBuySecondSellProfit
